f8aebab3394cccc52f9608b5504eb3b8dfbd021d
IlorDash <ilordash02@gmail.com>
2023-12-20 23:56:55 +0300

    Add base external with base package

---
+++ base_external/package/base-package/base-package.mk
@@ -0,0 +1,16 @@
+
+##############################################################
+#
+# BASE PACKAGE
+#
+##############################################################
+
+# ***_VERSION =
+# ***_SITE =
+# ***_SITE_METHOD = git
+
+# define ***_BUILD_CMDS
+# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/your-folder all
+# endef
+
+$(eval $(generic-package))
